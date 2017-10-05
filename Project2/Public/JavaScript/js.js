0cb3eb059da48246b6e4fd0a2bb41a695a3993df
PilotCo <cody.pilot@outlook.com>
2017-10-04 20:46:50 -0600

    updating the create user and login dropdowns

--- Project2/Public/JavaScript/js.js
+++ Project2/Public/JavaScript/js.js
@@ -1,9 +1,21 @@
 function login(){
 	$('#loginPopup').attr('class', 'displayLogin center');
-	$('#loginPopup').append("<h2 class='center' id='loginPrompt'>Please enter your username and password.</h2>");
-	$('#loginPopup').append("Username: " + "<input class='center' type='text' name='username'>");
+	$('#loginPopup').append("<h2 class='center' id='loginPrompt'>Please enter your email and password.</h2>");
+	$('#loginPopup').append("Email: " + "<input class='center' type='text' name='email'>");
 	$('#loginPopup').append("Password: " + "<input class='center' type='password' name='password'>");
 	$('#loginPopup').append("<button class='center' type='button'>LOGIN</button>");
+	$('#loginPopup').append("<p class='center' id='closeWindow' onclick='closeWindow()'>Close</p>");
+};
+
+function newUser(){
+	$('#loginPopup').attr('class', 'displayLogin center');
+	$('#loginPopup').append("<h2 class='center' id='createUserPrompt'>Enter all user information.</h2>");
+	$('#loginPopup').append("Company: " + "<input class='center' type='text' name='company'>");
+	$('#loginPopup').append("First Name: " + "<input class='center' type='text' name='fname'>");
+	$('#loginPopup').append("Last Name: " + "<input class='center' type='text' name='lname'>");
+	$('#loginPopup').append("Email: " + "<input class='center' type='text' name='email'>");
+	$('#loginPopup').append("Password: " + "<input class='center' type='password' name='password'>");
+	$('#loginPopup').append("<button class='center' type='button'>CREATE</button>");
 	$('#loginPopup').append("<p class='center' id='closeWindow' onclick='closeWindow()'>Close</p>");
 };
 
