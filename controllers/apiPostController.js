f4c5fb97fba5b7fdbf6c47db4d87e0a8d5588652
Nidant <Nidant2@gmail.com>
2017-10-09 18:21:56 -0600

    Added Scotts code
    Added Scotts code

--- controllers/apiPostController.js
+++ controllers/apiPostController.js
@@ -2,28 +2,84 @@ const db = require('../models');
 
 // -- new user (fname, lname, username, company, password)
 exports.newUser = function (req, res){
-		
-	};
+
+	let fName = req.body.fName,
+		lName = req.body.lName,
+		email = req.body.email,
+		password = req.body.password,
+		company = req.body.company,
+		team = req.body.team;
+
+	db.Users.create({
+		fName: fName,
+		lName: lName,
+		email: email,
+		password: password,
+		company: company,
+		team: team
+	})
+	.then(function(result) {
+		res.json(result);
+	});
+};
 
 // -- time punch(type, name, startDate, endDate) type = Company, User, Team
 exports.newTimePunch = function (req, res){
-		
-	};
+
+	let start = req.body.start,
+		stop = req.body.stop,
+		note = req.body.note;
+
+	db.Punchs.create({
+		start: start,
+		stop: stop,
+		note: note
+	})
+	.then(function(result) {
+			res.json(result);
+	});
+};
 
 // -- new job/task(job, Task)
 exports.newJob = function (req, res){
-		
-	};
+
+	let job = req.body.job,
+		task = req.body.task;
+
+	db.Jobs.create({
+		job: job,
+		task: task
+	})
+	.then(function(result) {
+		res.json(result);
+	});
+};
 
 // -- addHoliday Hours (type, name, hours, date) type = Company, User, Team
-exports.holiday = function (req, res){
-		db.users.create({
 
-		})
-	};
+// exports.holiday = function (req, res){
+// 		db.Users.create({
+// 			where: {
+// 				company: req.body.company,
+// 				include: {
+// 					model: Punchs,
+// 					include: {
+// 						model: Jobs
+// 					}
+// 				}
+// 			}
+// 		})
+// 	};
+
+// exports.holiday = function (req, res){
+// 		db.users.create({
+
+// 		})
+// 	};
+
 
 
 // -- join Team
-exports.addTeamMember = function (req, res){
+// exports.addTeamMember = function (req, res){
 		
-	};
+// 	};
